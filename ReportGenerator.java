12a4bc29a292f5d01dff4270ac76222076423065
Izhan10 <164905315+Izhan10@users.noreply.github.com>
2026-06-15 09:12:41 +0500

    Add files via upload

---
+++ ReportGenerator.java
@@ -0,0 +1,9 @@
+public class ReportGenerator {
+    public void generate(String category) {
+        if (category.equalsIgnoreCase("REFERENCE")) {
+            System.out.println("Reference book usage report generated.");
+        } else {
+            System.out.println("General book usage report generated.");
+        }
+    }
+}
