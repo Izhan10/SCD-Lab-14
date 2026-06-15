6f7af4b685da4ce4a38648339f4abce48ff41ab6
Izhan10 <164905315+Izhan10@users.noreply.github.com>
2026-06-15 09:23:22 +0500

    Update ReportGenerator.java

--- ReportGenerator.java
+++ ReportGenerator.java
@@ -1,7 +1,7 @@
 public class ReportGenerator {
     public void generate(String category) {
-        if (category.equalsIgnoreCase("REFERENCE")) {
-            System.out.println("Reference book usage report generated.");
+        if (category.equalsIgnoreCase("Reference")) {
+            System.out.println("Reference book usage has been report generated.");
         } else {
             System.out.println("General book usage report generated.");
         }
